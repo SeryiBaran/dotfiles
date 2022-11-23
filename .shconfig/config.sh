57d13429a327cecbdae0f42afdfef5e7f909c4ef
SeryiBaran <seryibaranminepy@yandex.ru>
2022-11-23 13:48:05 +0300

    $HISTSIZE = 1_000_000

--- .shconfig/config.sh
+++ .shconfig/config.sh
@@ -4,13 +4,13 @@ if [ $TILIX_ID ] || [ $VTE_VERSION ]; then
     source /etc/profile.d/vte-2.91.sh
 fi
 
+export HISTSIZE=1000000
 export EDITOR="micro -colorscheme my"
 
 [[ -f ~/.shconfig/functions.sh ]] && source ~/.shconfig/functions.sh
 [[ -f ~/.shconfig/aliases.sh ]] && source ~/.shconfig/aliases.sh
 
-export N_PREFIX="$HOME/n"; [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"  # Added by n-install (see http://git.io/n-install-repo).
-
-export PATH="$PATH:$HOME/zx-scripts:$HOME/sb-tools"
-
 # eval "$(oh-my-posh init $(oh-my-posh get shell) --config ~/omp.json)"
+
+export N_PREFIX="$HOME/n"; [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"  # Added by n-install (see http://git.io/n-install-repo).
+export PATH="$PATH:$HOME/zx-scripts:$HOME/sb-tools"
