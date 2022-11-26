13c76e70bd8d2de44cd2f7b3b11e11f682baa379
SeryiBaran <seryibaranminepy@yandex.ru>
2022-11-26 10:53:19 +0300

    fix

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,12 +2,12 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-if test -e ~/.shconfig/aliases.sh
-    source ~/.shconfig/aliases.sh
-end
-
 set HISTSIZE 1000000
 set EDITOR "micro -colorscheme my"
 
 set -a PATH "$N_PREFIX/bin"
 set -a PATH "$HOME/.shconfig/tools"
+
+if test -e ~/.shconfig/aliases.sh
+    source ~/.shconfig/aliases.sh
+end
