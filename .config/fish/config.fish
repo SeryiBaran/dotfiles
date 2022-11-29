345360aa9c617fb157fdfbd91724220b4d2ca387
SeryiBaran <seryibaranminepy@yandex.ru>
2022-11-29 13:09:08 +0300

    fish -a

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,7 +7,7 @@ export EDITOR="micro -colorscheme my"
 
 set N_PREFIX "$HOME/n"
 
-set -U fish_user_paths $fish_user_paths $N_PREFIX/bin $HOME/.shconfig/tools
+set -Ua fish_user_paths $N_PREFIX/bin $HOME/.shconfig/tools
 
 if test -e ~/.shconfig/aliases.sh
     source ~/.shconfig/aliases.sh
