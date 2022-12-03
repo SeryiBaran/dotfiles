9334cfa7859e29b7bb1efb350dec7f93f7b05a62
SeryiBaran <seryibaranminepy@yandex.ru>
2022-12-03 15:14:37 +0300

    pure config from ed62009

--- .shconfig/pure.sh
+++ .shconfig/pure.sh
@@ -1,4 +1,11 @@
 fpath+=($HOME/.zsh/pure)
 autoload -U promptinit; promptinit
 
+PURE_PROMPT_SYMBOL="$"
+
+zstyle :prompt:pure:path color "#059FFF"
+zstyle :prompt:pure:git:branch color "#00FF00"
+zstyle :prompt:pure:prompt:success color "#00FF00"
+zstyle :prompt:pure:virtualenv color "#C9C9C9"
+
 prompt pure
